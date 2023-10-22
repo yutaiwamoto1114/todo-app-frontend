3c285d1cf4db03235b6c3ea1b3be03e868391e01
Yuta Iwamoto <yutaiwamoto1114@gmail.com>
2023-10-22 22:10:23 +0900

    task ui changed

--- src/components/Todo.tsx
+++ src/components/Todo.tsx
@@ -1,6 +1,11 @@
-// frontend/src/components/Todo.tsx
-import { Button, ListItem, ListItemText, ListItemSecondaryAction, IconButton } from '@mui/material';
-import DeleteIcon from '@mui/icons-material/Delete';
+import {
+  Button,
+  ListItem,
+  ListItemText,
+  ListItemSecondaryAction,
+  IconButton,
+} from "@mui/material";
+import DeleteIcon from "@mui/icons-material/Delete";
 
 interface TodoProps {
   task: { id: number; title: string; completed: boolean };
@@ -10,12 +15,35 @@ interface TodoProps {
 
 const Todo: React.FC<TodoProps> = ({ task, toggleCompleted, deleteTask }) => {
   return (
-    <ListItem>
-      <ListItemText primary={task.title} style={task.completed ? { textDecoration: 'line-through' } : {}} />
+    <ListItem
+      style={{
+        border: "1px solid gray",
+        boxShadow: "2px 2px 5px rgba(0, 0, 0, 0.1)",
+        marginBottom: "8px",
+        borderRadius: "4px",
+      }}
+    >
+      {/* ○のような完了ボタンを追加 */}
+      <Button
+        onClick={toggleCompleted}
+        style={{
+          margin: "0 8px 0 0",
+          background: task.completed ? "green" : "transparent",
+          border: "2px solid green",
+          borderRadius: "50%",
+          height: "25px",
+          width: "25px",
+        }}
+      />
+      <ListItemText
+        primary={task.title}
+        style={
+          task.completed
+            ? { textDecoration: "line-through", color: "gray" }
+            : {}
+        }
+      />
       <ListItemSecondaryAction>
-        <Button onClick={toggleCompleted}>
-          {task.completed ? '未完了にする' : '完了'}
-        </Button>
         <IconButton edge="end" aria-label="delete" onClick={deleteTask}>
           <DeleteIcon />
         </IconButton>
